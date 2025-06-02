71df936541c88057c89a693b5cf168c6592dbed7
sunggyu-jeong <jskoder0109@gmail.com>
2025-06-02 14:11:37 +0900

    feat. pinia 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module', '@nuxt/eslint'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/eslint-module',
+    '@nuxt/eslint',
+    '@pinia/nuxt'
+  ],
   eslint: {
     lintOnStart: true,
     fix: true,
